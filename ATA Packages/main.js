1eea93f6f48da936bafc3d8bd9e35b7ad968efae
“Nicole <nifloyd@icloud.com>
2018-04-11 13:35:12 -0400

    -

--- ATA Packages/main.js
+++ ATA Packages/main.js
@@ -1,9 +1,22 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
+
+ function mobileDropdown() {
+    console.log("main my function")
+     var x = document.getElementById("myTopnav");
+     console.log(x.className);
+     if (x.className === "topnav") {
+         x.className += " responsive";
+     } else {
+         x.className = "topnav";
+    } 
 }
+
+// function mobileDropdown() {
+//     console.log('inside myFunction');
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "topnav") {
+//         x.className += " responsive";
+//     } else {
+//         x.className = "topnav";
+//     }
+// }
